14ae8cbd326855811e6eee3f6d9b366e5272cdde
Alexandre Possari <ac.possari@gmail.com>
2024-10-04 17:36:50 -0300

    Initial commit

---
+++ matchManager/matchManager/src/test/java/matchManager/MatchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package matchManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
