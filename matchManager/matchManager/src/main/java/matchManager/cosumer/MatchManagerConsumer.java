1721692a67d4ffe60fd539f47724a98561b215b4
fiddelis <lucas.ruan@ges.inatel.br>
2024-10-31 18:23:28 -0300

    feat: fila Rabbitmq adicionado e configurado

--- matchManager/matchManager/src/main/java/matchManager/cosumer/MatchManagerConsumer.java
+++ matchManager/matchManager/src/main/java/matchManager/cosumer/MatchManagerConsumer.java
@@ -1,4 +1,34 @@
 package matchManager.cosumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import matchManager.model.match.Match;
+import matchManager.model.user.User;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MatchManagerConsumer {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @RabbitListener(queues = "user-queue")
+    public void userReceive(String message) {
+        try {
+            User user = objectMapper.readValue(message, User.class);
+            System.out.println(user.toString());
+            // TODO: Lógica do Service
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RabbitListener(queues = "match-queue")
+    public void matchReceive(String message) {
+        try {
+            Match match = objectMapper.readValue(message, Match.class);
+            System.out.println(match.toString());
+            // TODO: Lógica do Service
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- matchManager/matchManager/src/main/java/matchManager/producer/MatchManagerProducer.java
+++ matchManager/matchManager/src/main/java/matchManager/producer/MatchManagerProducer.java
@@ -1,4 +1,33 @@
 package matchManager.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import matchManager.model.match.Match;
+import matchManager.model.user.User;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MatchManagerProducer {
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    // Envio na fila de usuarios
+    public void sendUser(User user) throws JsonProcessingException {
+        amqpTemplate.convertAndSend(
+                "user-queue",
+                objectMapper.writeValueAsString(user)
+        );
+    }
+
+    // Envio na fila de partidas
+    public void sendMatch(Match match) throws JsonProcessingException {
+        amqpTemplate.convertAndSend(
+                "match-queue",
+                objectMapper.writeValueAsString(match)
+        );
+    }
 }
