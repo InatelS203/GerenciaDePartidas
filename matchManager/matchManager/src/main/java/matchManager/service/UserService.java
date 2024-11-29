dc6ada590ad1ed2cc61574abefea93b765606639
Pedro Freitas <phfreitas@ges.inatel.br>
2024-11-28 22:22:51 -0300

    feat: implements `UserService`

--- matchManager/matchManager/src/main/java/matchManager/service/UserService.java
+++ matchManager/matchManager/src/main/java/matchManager/service/UserService.java
@@ -1,5 +1,7 @@
 package matchManager.service;
 
+import matchManager.model.user.User;
+import matchManager.producer.MatchManagerProducer;
 import matchManager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private MatchManagerProducer matchManagerProducer;
+
+    public void updateUser(User user) {
+        userRepository.save(user);
+
+        matchManagerProducer.sendUserScore(user);
+    }
 }
